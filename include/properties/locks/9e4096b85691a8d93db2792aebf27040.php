da85a626946358177efe592eb3f6b2ea1b377190
berckutkirill <berckutkirill@ya.ru>
2018-03-26 17:18:17 +0300

    bronza

--- include/properties/locks/9e4096b85691a8d93db2792aebf27040.php
+++ include/properties/locks/9e4096b85691a8d93db2792aebf27040.php
@@ -7,8 +7,14 @@
                     <p class="price dp-setfurn__title">+ <span class="num"><?=$arParams['PRICES'][15]['PRICE']?></span> руб. за комплект</p>
                 </div>
                 <div class="dp-setfurn__item-top">
-                    <div class="dp-setfurn__lin"><img src="<?= SITE_TEMPLATE_PATH ?>/img/cool/furn-bronze-a.jpg"/></div>
-                    <div class="dp-setfurn__lin"><img src="<?= SITE_TEMPLATE_PATH ?>/img/cool/furn-bronze-l.jpg"/></div>
+                    <div class="dp-setfurn__lin">
+                        <img src="<?= SITE_TEMPLATE_PATH ?>/img/cool/furn-bronze-a.jpg"/>
+                        <span class="dp-furn-title">Броненакладка Crit на цилиндр</span>
+                    </div>
+                    <div class="dp-setfurn__lin">
+                        <img src="<?= SITE_TEMPLATE_PATH ?>/img/cool/furn-bronze-l.jpg"/>
+                        <span class="dp-furn-title">Накладка Crit</br> на сувальду</span>
+                    </div>
                 </div>
                 <div class="dp-setfurn__item-bot">
                     <img src="<?= SITE_TEMPLATE_PATH ?>/img/cool/hand-arm-bronz.jpg"/>
